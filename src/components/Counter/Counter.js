9fd962c922656b49d0d89b61e9e9ccdf39ce6a6d
niteshsharma5 <2018ucp1030@mnit.ac.in>
2021-10-27 09:09:40 +0530

    fix:counter & name micro-projects

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -13,13 +13,12 @@ const Counter = () => {
 	return (
 		<div className="main">
 			<h3>Counter</h3>
-			<button onClick={increment}>Increment</button>
 			<span>{count}</span>
-			<button onClick={decrement}>Decrement</button>
-			<br />
-			<button className="right_side" onClick={reset}>
-				Reset
-			</button>
+			<div id="counter-controls">
+				<button onClick={increment}>Increment</button>
+				<button onClick={decrement}>Decrement</button>
+				<button onClick={reset}>Reset</button>
+			</div>
 		</div>
 	);
 };
--- src/components/Name/Name.js
+++ src/components/Name/Name.js
@@ -2,28 +2,42 @@ import React, { useState } from "react";
 import "./Name.css";
 
 const Name = () => {
-	const [name, setName] = useState();
-	const [name1, setName1] = useState();
+	const [name, setName] = useState("");
+	const [submittedName, setSubmittedName] = useState("");
 
 	const handleChange = (e) => setName(e.target.value);
 
-	const handleChangeOnSubmit = (e) => setName1(name);
+	const handleChangeOnSubmit = (e) => {
+		e.preventDefault();
+		setSubmittedName(name);
+	};
 
 	return (
 		<div className="main">
 			<h3>Name</h3>
-			<label>Your Name </label>
-			<input
-				type="text"
-				placeholder="Please Enter Your Name"
-				value={name}
-				onChange={handleChange}
-			/>
-			<button className="submit_btn" onClick={handleChangeOnSubmit}>
-				Submit
-			</button>
-			{name.length > 0 && <div className="name_display">Hello {name}</div>}
-			{name1.length > 0 && <div className="name_display">Hello {name1}</div>}
+			<form id="name-controllers" onSubmit={handleChangeOnSubmit}>
+				<label>Your Name </label>
+				<input
+					name="my-name"
+					type="text"
+					placeholder="Please Enter Your Name"
+					value={name}
+					onChange={handleChange}
+				/>
+				<button
+					type="submit"
+					className="submit-btn"
+					onClick={handleChangeOnSubmit}
+				>
+					Submit
+				</button>
+			</form>
+			<div id="name-values">
+				{name.length > 0 && <div className="name-display">Hello {name}</div>}
+				{submittedName.length > 0 && (
+					<div className="name-display">Hello {submittedName}</div>
+				)}
+			</div>
 		</div>
 	);
 };
