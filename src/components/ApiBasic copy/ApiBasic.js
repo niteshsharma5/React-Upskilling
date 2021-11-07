6ca28b242437271d6c3c391eb23929396e30d3ed
niteshsharma5 <2018ucp1030@mnit.ac.in>
2021-11-07 10:45:20 +0530

    feat: API Basic micro project

--- src/components/ApiBasic copy/ApiBasic.js
+++ src/components/ApiBasic copy/ApiBasic.js
@@ -3,7 +3,6 @@ import "./ApiBasic.css";
 
 const ApiBasic = () => {
 	const [users, setUsers] = useState([]);
-	const [dataIsLoaded, setDataIsLoaded] = useState(false);
 
 	const getUserInformation = (event) => {
 		event.preventDefault();
@@ -11,9 +10,8 @@ const ApiBasic = () => {
 		fetch("https://jsonplaceholder.typicode.com/users")
 			.then((res) => res.json())
 			.then((json) => {
+				console.log(json);
 				setUsers(json);
-				console.log(users);
-				setDataIsLoaded(true);
 			})
 			.catch((err) => {
 				console.log(err);
@@ -24,36 +22,28 @@ const ApiBasic = () => {
 		<div>
 			<h3>API Basic</h3>
 			<button onClick={getUserInformation}>Fetch data</button>
-			<div className="display-details">
-				{dataIsLoaded && (
-					<table>
-						<tr>
-							<th>Name</th>
-							<th>E-mail</th>
-							<th>Phone</th>
-							<th>Address</th>
-							<th>Company</th>
-							<th>Website</th>
-						</tr>
-
-						{users.map((user) => (
+			<div>
+				<table>
+					{users.length > 0 && (
+							<tr>
+								<th>Name</th>
+								<th>E-mail</th>
+								<th>Phone</th>
+								<th>Address</th>
+								<th>Company</th>
+								<th>Month</th>
+							</tr>
+						) &&
+						users.map((user) => (
 							<tr key={user.id}>
 								<td>{user.name}</td>
 								<td>{user.email}</td>
 								<td>{user.phone}</td>
-								<td>
-									{user.address.street +
-										" " +
-										user.address.city +
-										" " +
-										user.address.zipcode}
-								</td>
-								<td>{user.company.name}</td>
-								<td>{user.website}</td>
+								<td>{user.address}</td>
+								<td>{user.company}</td>
 							</tr>
 						))}
-					</table>
-				)}
+				</table>
 			</div>
 		</div>
 	);
